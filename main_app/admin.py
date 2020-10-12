750bea8777aae885c6931f51ae54605d3e453e0a
pratik <engrpatel21@gmail.com>
2020-10-12 12:57:41 -0400

    user profile updated

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+    
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInLine,)
+    
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200)
+    pic = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"Photo for cat_id: {self.cat_id} @{self.url}"
+
+    
