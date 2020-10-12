a6b5ed9cff9617b3b24ca7509fd956b1b9517b51
pratik <engrpatel21@gmail.com>
2020-10-12 14:54:29 -0400

    css for profile page

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Profile
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def profile(request):
+    profile = Profile.objects.filter(user=request.user)
+    return render(request, 'profile/profile.html')
 
 def signup(request):
   error_message = ''
@@ -24,3 +30,7 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+class ProfileUpdate( UpdateView):
+    model = Profile
+    fields = '__all__'
